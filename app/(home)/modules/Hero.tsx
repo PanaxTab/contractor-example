5b5ea118ee5a37a8bba91021c283a138345adcf7
PanaxTab <panax36@gmail.com>
2024-09-17 09:59:58 +0300

    added UI elements and components, populated public folder

---
+++ app/(home)/modules/Hero.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Image from "next/image";
+import { Container } from "@/components/Container";
+import { Button } from "@/components/Button";
+import StarImg from "@/public/star.png";
+import Link from "next/link";
+import { FaArrowDown } from "react-icons/fa";
+
+export const Hero = () => {
+  return (
+    <div className="relative h-screen w-full pt-12 md:pt-0 overflow-hidden">
+      <Container className="relative z-10 flex flex-col justify-between h-full text-white">
+        <div />
+        <div className="flex gap-2">
+          <div className="flex items-center gap-1">
+            {[...Array(5)].map((_, i) => (
+              <Image
+                key={`star${i}`}
+                src={StarImg}
+                alt="star"
+                width={16}
+                height={16}
+              ></Image>
+            ))}
+          </div>
+          <p className="text-sm text-white font-bold">25+ succesful projects</p>
+        </div>
+        <div>
+          <h1 className="my-4">
+            WE BUILD WITH <br />
+            <span className="text-primary">PRECISION</span>
+          </h1>
+          <p className="mb-8">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+            rerum, corporis possimus voluptates ab atque. Quos ipsa perferendis
+            non molestias dignissimos, nesciunt illo iure ab nihil deserunt hic
+            quam corrupti?
+          </p>
+          <Button />
+        </div>
+        <div>
+            <Link href="/" className="rounded-full p-2 bg-transparent text-white border-2 border-white shadow-lg animate-bounce">
+                <FaArrowDown size="24">
+                </FaArrowDown>
+            </Link>
+        </div>
+      </Container>
+    </div>
+  );
+};
---
+++ app/(home)/modules/HeroCarousel.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect } from "react";
+
+
+const images = ["/parapija.jpg", "/hero2.png", "/hero3.png"];
+
+export const HeroCarousel = () => {
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+
+  const handleImageChange = (newIndex: number) => {
+    if (newIndex !== currentImageIndex) {
+      setCurrentImageIndex(newIndex);
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      handleImageChange((currentImageIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [currentImageIndex]);
+
+  return (
+    <>
+      <div className="absolute inset-0 overflow-hidden">
+        {images.map((img, index) => (
+          <div
+            key={img}
+            className={`absolute inset-0 transition-all duration-1000 ${
+              index === currentImageIndex
+                ? "opacity-100 scale-105"
+                : "opacity-0 scale-100"
+            }`}
+          >
+            <Image
+              src={img}
+              alt={`Carousel Image ${index + 1}`}
+              fill
+              className="object-cover object-center"
+              priority={index === currentImageIndex}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
